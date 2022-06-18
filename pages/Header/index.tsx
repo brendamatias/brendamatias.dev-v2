c4479d727e62a71896f59b96dec7a383fdcadfb8
Brenda Matias <brendamatias.sobral@gmail.com>
2022-06-18 18:03:25 -0300

    feat: add header component

--- pages/Header/index.tsx
+++ pages/Header/index.tsx
@@ -1,11 +1,56 @@
 import type { NextPage } from 'next';
 
-import { Container } from './styles';
+import Image from 'next/image';
+import { MdOutlineArrowForward } from 'react-icons/md';
+import { BsMouse } from 'react-icons/bs';
+import { IoIosArrowRoundDown } from 'react-icons/io';
+import { Container, List } from './styles';
+import logo from '../../assets/logo.png';
 
-const Header: NextPage = () => (
-  <Container>
-    <span>Header</span>
-  </Container>
-);
+const Header: NextPage = () => {
+  const links = ['about', 'experience', 'projects', 'contact'];
+
+  return (
+    <Container>
+      <div className="icons">
+        <BsMouse />
+        <IoIosArrowRoundDown />
+      </div>
+      <nav>
+        <Image src={logo} alt="Brenda Matias Logo" />
+
+        <List>
+          {links.map((link, index) => (
+            <li key={link}>
+              <a href={`#${link}`} className={`${index === 0 && 'active'}`}>
+                <strong>0{index + 1}.</strong> {link}
+              </a>
+            </li>
+          ))}
+          <li>
+            <button type="button">
+              resume <MdOutlineArrowForward />
+            </button>
+          </li>
+        </List>
+      </nav>
+
+      <section>
+        <div>
+          <h1>hey! i'm Brenda Matias.</h1>
+          <h1>Full Stack Software Enginner.</h1>
+
+          <p>
+            I am passionate about software development, where I seek to improve myself in technologies that simplify and
+            evolve people's daily lives. I currently reside in Recife, Pernambuco and have experience in full stack
+            development.
+          </p>
+
+          <button type="button">see my projects</button>
+        </div>
+      </section>
+    </Container>
+  );
+};
 
 export default Header;
--- pages/Header/styles.ts
+++ pages/Header/styles.ts
@@ -1,8 +1,65 @@
 import styled from 'styled-components';
 
 export const Container = styled.header`
+  position: relative;
+
+  > nav {
     display: flex;
     align-items: center;
     justify-content: space-between;
-    padding: 1rem 2rem;
+    padding: 0 2rem;
+  }
+
+  > section {
+    display: flex;
+    align-items: center;
+    height: calc(100vh - 198px);
+    color: #222222;
+
+    > div {
+      padding: 1.5rem 2rem;
+
+      > h1 {
+        font-size: 3rem;
+        margin: 0;
+        padding: 0;
+      }
+
+      > p {
+        max-width: 25rem;
+        text-align: justify;
+        margin-bottom: 2rem;
+      }
+    }
+  }
+
+  > .icons {
+    display: flex;
+    flex-direction: column;
+    gap: 4.5rem;
+    font-size: 20px;
+    position: absolute;
+    right: 2rem;
+    bottom: 0;
+  }
+`;
+
+export const List = styled.ul`
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+
+  > li {
+    > a {
+      color: #222222;
+      border-bottom: 3px solid transparent;
+      padding-bottom: 8px;
+      transition: 0.3s;
+
+      &:hover,
+      &.active {
+        border-color: #fa2373;
+      }
+    }
+  }
 `;
