b58e842dfd14370893007500d6716274dace4e08
Brenda Matias <brendamatias.sobral@gmail.com>
2022-06-19 18:22:40 -0300

    feat: add header responsivity

--- pages/Header/index.tsx
+++ pages/Header/index.tsx
@@ -2,38 +2,52 @@ import type { NextPage } from 'next';
 
 import Image from 'next/image';
 import { MdOutlineArrowForward } from 'react-icons/md';
-import { BsMouse } from 'react-icons/bs';
-import { IoIosArrowRoundDown } from 'react-icons/io';
-import { Container, List } from './styles';
+// import { BsMouse } from 'react-icons/bs';
+// import { IoIosArrowRoundDown } from 'react-icons/io';
+import { IoMdMenu, IoMdClose } from 'react-icons/io';
+import { useState } from 'react';
+import { Container, Nav } from './styles';
 import logo from '../../assets/logo.png';
 
 const Header: NextPage = () => {
   const links = ['about', 'experience', 'projects', 'contact'];
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
 
   return (
     <Container>
-      {/* <div className="icons">
+      <div>
+        {/* <div className="icons">
         <BsMouse />
         <IoIosArrowRoundDown />
       </div> */}
-      <nav>
-        <Image src={logo} alt="Brenda Matias Logo" />
 
-        <List>
-          {links.map((link, index) => (
-            <li key={link}>
-              <a href={`#${link}`} className={`${index === 0 && 'active'}`}>
-                <strong>0{index + 1}.</strong> {link}
-              </a>
-            </li>
-          ))}
-          <li>
-            <button type="button" className="button">
-              resume <MdOutlineArrowForward />
-            </button>
-          </li>
-        </List>
-      </nav>
+        <nav>
+          <Image src={logo} alt="Brenda Matias Logo" width={50} height={50} quality="100%" />
+
+          <Nav>
+            <div className="menuBurger">
+              <button type="button" onClick={() => setIsOpenMenu(!isOpenMenu)}>
+                {isOpenMenu ? <IoMdClose /> : <IoMdMenu />}
+              </button>
+            </div>
+
+            <ul className={`${isOpenMenu && 'visible'}`}>
+              {links.map((link, index) => (
+                <li key={link}>
+                  <a href={`#${link}`} className={`${index === 0 && 'active'}`}>
+                    <strong>0{index + 1}.</strong> {link}
+                  </a>
+                </li>
+              ))}
+              <li>
+                <button type="button" className="button">
+                  resume <MdOutlineArrowForward />
+                </button>
+              </li>
+            </ul>
+          </Nav>
+        </nav>
+      </div>
     </Container>
   );
 };
--- pages/Header/styles.ts
+++ pages/Header/styles.ts
@@ -1,49 +1,128 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const Container = styled.header`
   background: linear-gradient(90deg, #fff 70%, #fff0fc 40%);
+  width: 100%;
 
-  > nav {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    width: 100%;
-    padding: 2rem;
-    max-width: 1100px;
-    margin: 0 auto;
-    z-index: 999;
+  @media (max-width: 900px) {
+    position: fixed;
+    z-index: 99999;
+    background: #fff;
   }
 
-  > .icons {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-    gap: 4.5rem;
-    font-size: 20px;
-    position: fixed;
-    width: 100%;
-    padding: 2rem;
+  > div {
     max-width: 1100px;
-    bottom: 4rem;
+    margin: 0 auto;
+    padding: 2rem;
+    z-index: 999;
+    position: relative;
+
+    @media (max-width: 900px) {
+      padding: 1rem 2rem;
+    }
+
+    > nav {
+      position: relative;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+    > .icons {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-end;
+      gap: 4.5rem;
+      font-size: 20px;
+      position: fixed;
+      width: 100%;
+      padding: 2rem;
+      max-width: 1100px;
+      bottom: 4rem;
+    }
   }
 `;
 
-export const List = styled.ul`
-  display: flex;
-  align-items: center;
-  gap: 2rem;
+export const Nav = styled.div`
+  ${({ theme }) => css`
+    > ul {
+      display: flex;
+      align-items: center;
+      gap: 2rem;
 
-  > li {
-    > a {
-      color: #222222;
-      border-bottom: 3px solid transparent;
-      padding-bottom: 8px;
-      transition: 0.3s;
+      > li {
+        > a {
+          color: #222222;
+          border-bottom: 3px solid transparent;
+          padding-bottom: 8px;
+          transition: 0.3s;
 
-      &:hover,
-      &.active {
-        border-color: #fa2373;
+          &:hover,
+          &.active {
+            border-color: #fa2373;
+          }
+        }
       }
     }
-  }
+
+    .menuBurger {
+      display: none;
+
+      svg {
+        transition: all 0.2s;
+        font-size: 1.5rem;
+        color: ${theme.colors.gray};
+
+        &:hover {
+          color: ${theme.colors.primary};
+        }
+      }
+    }
+
+    @media (max-width: 900px) {
+      position: absolute;
+      top: 0;
+      right: 0;
+
+      > ul {
+        flex-direction: column;
+        align-items: flex-end;
+        background-color: #fff;
+        box-shadow: 0 10px 53px 0 rgba(72, 49, 212, 0.05);
+        font-size: 0.875rem;
+        transition: box-shadow 0.4s;
+        padding: 5rem 2rem 2rem 2rem;
+        border-radius: 0.3rem;
+        display: none;
+      }
+
+      .menuBurger {
+        display: block;
+        position: absolute;
+        top: 1.5rem;
+        right: 2rem;
+        z-index: 2;
+      }
+
+      .visible {
+        display: flex;
+      }
+      /* &::after {
+      content: 'X';
+    } */
+
+      /* display: none; */
+      /* position: absolute;
+    align-items: flex-end;
+    flex-direction: column;
+    top: 84px;
+    right: 32px;
+    background-color: #fff;
+    font-size: 0.875rem;
+    transition: 0.3s;
+
+    > li {
+    } */
+    }
+  `}
 `;
